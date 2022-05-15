c74136a8b9ac43836ba036155b3bc9a83c24d182
IT21339806 <102345433+IT21339806@users.noreply.github.com>
2022-05-15 12:39:25 +0000

    abcd

---
+++ service.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+confirmation_letter cl1,cl2,cl3
+{
+confirmation_letter::confirmation_letter() {
+cout << "Default Constructor confirmation_letter() called" << endl;
+}
+cl1.sendconfirmationletter();
+cl2.sendconfirmationletter();
+cl3.sendconfirmationletter(;)
+confirnation_letter::confirmation_letter()
+{
+cout << "Destructed" << endl;
+}
+}
+}
